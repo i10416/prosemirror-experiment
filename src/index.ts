8aa84243f9802c140203b99c6768042747cdf6f5
i10416 <ito.yo16uh90616@gmail.com>
2025-01-16 22:34:05 +0900

    explore: roundtrip JSON ser-de

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,32 @@
-import { Schema } from "prosemirror-model"
+import { Schema, Node } from "prosemirror-model"
 import { EditorState } from "prosemirror-state"
 
-
+const schema = new Schema(
+    {
+        nodes: {
+            doc: {
+                content: "block+"
+            },
+            paragraph: {
+                content: "inline*",
+                group: "block"
+            },
+            text: {
+                group: "inline"
+            }
+        }
+    }
+)
 const state = EditorState.create(
     {
-        schema: new Schema(
-            {
-                nodes: {
-                    doc: {
-                        content: "block+"
-                    },
-                    paragraph: {
-                        content: "inline*",
-                        group: "block"
-                    },
-                    text: {
-                        group: "inline"
-                    }
-                }
-            }
-        )
+        schema: schema
     }
 )
 console.log(state.doc.content)
 const tx = state.tr.insertText("Hello, ProseMirror!")
 const next = state.apply(tx)
 console.log(next.doc.textContent)
+const forward = next.doc.toJSON()
+const back = Node.fromJSON(schema, forward)
+console.log(forward)
+console.log(back.toJSON())

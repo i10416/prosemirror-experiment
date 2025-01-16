48275d8e6b483181b15de40ae524282a2dcb1f30
i10416 <ito.yo16uh90616@gmail.com>
2025-01-17 00:43:48 +0900

    tools: apply biome formatter

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,26 @@
-import { Schema, Node, Slice, Fragment } from "prosemirror-model"
-import { EditorState } from "prosemirror-state"
+import { Schema, Node, Slice, Fragment } from "prosemirror-model";
+import { EditorState } from "prosemirror-state";
 
-const schema = new Schema(
-    {
-        nodes: {
-            doc: {
-                content: "text*"
-            },
-            text: {}
-        }
-    }
-)
-const state = EditorState.create(
-    {
-        schema: schema
-    }
-)
-console.log(state.doc.content)
-const tx = state.tr.insertText("Hello, ProseMirror!")
-const next = state.apply(tx)
-console.log(next.doc.textContent)
-const forward = next.doc.toJSON()
-const back = Node.fromJSON(schema, forward)
-console.log(forward)
-console.log(back.toJSON())
+const schema = new Schema({
+	nodes: {
+		doc: {
+			content: "text*",
+		},
+		text: {},
+	},
+});
+const state = EditorState.create({
+	schema: schema,
+});
+console.log(state.doc.content);
+const tx = state.tr.insertText("Hello, ProseMirror!");
+const next = state.apply(tx);
+console.log(next.doc.textContent);
+const forward = next.doc.toJSON();
+const back = Node.fromJSON(schema, forward);
+console.log(forward);
+console.log(back.toJSON());
 
-const nextTx = next.tr.replace(0, 5, new Slice(
-    Fragment.empty, 0, 0
-))
-const nextX2 = next.apply(nextTx)
-console.log(nextX2.doc.toJSON())
-
+const nextTx = next.tr.replace(0, 5, new Slice(Fragment.empty, 0, 0));
+const nextX2 = next.apply(nextTx);
+console.log(nextX2.doc.toJSON());

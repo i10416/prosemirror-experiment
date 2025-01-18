21e53dd5fee4aba5ab31ec6c11ee1a29a29e3247
i10416 <ito.yo16uh90616@gmail.com>
2025-01-18 15:37:59 +0900

    tidy: use Plugin interface instead of direct implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Schema } from "prosemirror-model";
-import { EditorState, TextSelection } from "prosemirror-state";
+import { EditorState, TextSelection, Plugin } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 const schema = new Schema({
 	nodes: {
@@ -22,34 +22,32 @@ const state = EditorState.create({
 });
 const tx = state.tr.insertText(
 	"This is a ProseMirror editor without plugins " +
-		"built mainly for understanding its data flow. " +
-		"I'm not sure why, but you need to hit `Enter` twice " +
-		"and move a cursor down when you are at the end of paragraph.",
+	"built mainly for understanding its data flow. " +
+	"I'm not sure why, but you need to hit `Enter` twice " +
+	"and move a cursor down when you are at the end of paragraph.",
 );
 const next = state.apply(tx);
 
+const naiveNewBlockPlugin = new Plugin({
+	props: {
+		handleKeyDown: (view, event) => {
+			if (event.key == "Enter") {
+				const s = view.state;
+				const nextNode = s.selection.$to.parent.type.createAndFill()!;
+				const to = s.selection.$to;
+				const tr = s.tr.insert(to.pos, nextNode);
+				const sel = TextSelection.create(tr.doc, to.pos + 1);
+				tr.setSelection(sel);
+				view.dispatch(tr);
+				return false;
+			}
+		},
+	}
+})
+
 const view = new EditorView(document.getElementById("app"), {
 	state: next,
-	plugins: [
-		{
-			spec: {},
-			props: {
-				handleKeyDown: (view, event) => {
-					if (event.key == "Enter") {
-						const s = view.state;
-						const nextNode = s.selection.$to.parent.type.createAndFill()!;
-						const to = s.selection.$to;
-						const tr = s.tr.insert(to.pos, nextNode);
-						const sel = TextSelection.create(tr.doc, to.pos + 1);
-						tr.setSelection(sel);
-						view.dispatch(tr);
-						return false;
-					}
-				},
-			},
-			getState: (_) => {},
-		},
-	],
+	plugins: [naiveNewBlockPlugin],
 	dispatchTransaction(transaction) {
 		const newState = view.state.apply(transaction);
 		view.updateState(newState);
